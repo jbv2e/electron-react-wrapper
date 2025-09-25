5d041e18fc22900a5802c7a6f25153fb430f5ec2
jbv2 <j4petv2@gmail.com>
2025-09-25 14:43:15 +0900

    main.ts ipc 핸들러 분리

--- dist-electron/main.js
+++
@@ -1,9 +0,0 @@
-import "node:path";
-import "node:url";
-import "electron";
-import { M, R, V } from "./main-BhhAlzD_.js";
-export {
-  M as MAIN_DIST,
-  R as RENDERER_DIST,
-  V as VITE_DEV_SERVER_URL
-};
